592e600294d6d7ba36765a529d8168b83d9b6569
Jose Cuellar <jose.cuellar@zayo.com>
2026-02-12 15:07:12 -0600

    Fix/idioma-v1

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "¿Estás segura?",
+    "¿De verdad?",
+    "¿Estás segurísima?",
+    "Amorcito, por favor...",
+    "¡Piénsalo bien!",
+    "Si dices que no, me voy a poner muy triste...",
+    "Estaré muy triste...",
+    "Estaré muy, pero muy triste...",
+    "Bueno, ya dejo de preguntar...",
+    "Es broma, ¡dime que sí, porfi! ❤️"
 ];
 
 let messageIndex = 0;
